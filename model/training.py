64a0ce17a77ff87e420d1d706d967598a68bd3d0
Anthony Botticchio <anthonyjb5228@gmail.com>
2025-11-15 20:08:22 -0500

    Fixed errors.

--- model/training.py
+++ model/training.py
@@ -2,6 +2,11 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import joblib
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score,
+)
 import lightgbm as lgb
 import data
 
